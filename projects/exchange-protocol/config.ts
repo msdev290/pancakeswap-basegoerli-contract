a80004738d517d18e98ac04768746f9f2e322d9e
mind1995star <daisukenagata0209@gmail.com>
2023-09-26 16:04:12 +0900

    init: deployed smart contract

---
+++ projects/exchange-protocol/config.ts
@@ -0,0 +1,14 @@
+export default {
+  PancakeRouter: {
+    mainnet: "0x10ed43c718714eb63d5aa57b78b54704e256024e",
+    testnet: "0x0AA18992fC3a4105291fD4d1353B5DB16c601c9c",
+  },
+  MaxZapReverseRatio: {
+    mainnet: "50", // 0.5%
+    testnet: "50", // 0.5%
+  },
+  WBNB: {
+    mainnet: "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c",
+    testnet: "0x51EA39b52B7fC2aEf198ABFc27f76303298e3765",
+  },
+};
---
+++ projects/exchange-protocol/hardhat.config.ts
@@ -0,0 +1,99 @@
+import type { HardhatUserConfig, NetworkUserConfig } from "hardhat/types";
+import "@nomiclabs/hardhat-ethers";
+import "@nomiclabs/hardhat-web3";
+import "@nomiclabs/hardhat-truffle5";
+import "hardhat-abi-exporter";
+import "hardhat-contract-sizer";
+import "solidity-coverage";
+import "dotenv/config";
+
+const baseTestnet: NetworkUserConfig = {
+  url: "https://goerli.base.org/",
+  // chainId: 84531,
+  accounts: [process.env.KEY_TESTNET!],
+};
+
+const baseMainnet: NetworkUserConfig = {
+  url: "https://mainnet.base.org/",
+  chainId: 8453,
+  accounts: [process.env.KEY_MAINNET!],
+};
+
+const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {},
+    testnet: baseTestnet,
+    // mainnet: bscMainnet,
+  },
+  etherscan: {
+    apiKey: {
+      // Basescan doesn't require an API key, however
+      // Hardhat still expects an arbitrary string to be provided.
+      "base-goerli": "PLACEHOLDER_STRING"
+    },
+    customChains: [
+      {
+        network: "base-goerli",
+        chainId: 84531,
+        urls: {
+          apiURL: "https://api-goerli.basescan.org/api",
+          browserURL: "https://goerli.basescan.org"
+        }
+      }
+    ]
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.4",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 99999,
+          },
+        },
+      },
+      {
+        version: "0.6.6",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 99999,
+          },
+        },
+      },
+      {
+        version: "0.5.16",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 99999,
+          },
+        },
+      },
+      {
+        version: "0.4.18",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 99999,
+          },
+        },
+      },
+    ],
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  },
+  abiExporter: {
+    path: "./data/abi",
+    clear: true,
+    flat: false,
+  },
+};
+
+export default config;
